c97e916251665587df0cd92d06a5424c784f1944
Jennifer <jteissler@uga.edu>
2021-12-06 21:06:45 -0500

    Post History Hidden

--- plugin/hide-stats.js
+++ plugin/hide-stats.js
@@ -2,5 +2,4 @@ window.addEventListener('load', function () {
 	console.log(document.getElementsByClassName('special_mentions_visibility hide')[0]);
 	document.getElementsByClassName('special_mentions_visibility hide')[0].click();
 	console.log("Loaded Stat Hide");
-
 }, false);
