3357b27b54b4132855c88ce419a92e6697ea6d66
Jennifer <jteissler@uga.edu>
2021-12-06 19:07:26 -0500

    Donations Story
    also separated stories into individual files

---
+++ plugin/donation-close.js
@@ -0,0 +1,16 @@
+// Translates raw text into an actual HTML element
+function hitMeUp(html) {
+	let element = document.createElement('template');
+	element.innerHTML = html;
+	return element.content.firstChild;
+}
+
+let tag = hitMeUp('<div class="button-wrapper"><div class="button_pad close-button"><button>Close</button></div></div>');
+
+window.addEventListener('load', function () {
+  document.getElementsByClassName('explanation')[1].after(tag);
+	tag.children[0].children[0].addEventListener("click", function() { 
+		document.getElementById('contribution_wrapper').remove();
+	});
+	console.log("Loaded Donations");
+}, false);
