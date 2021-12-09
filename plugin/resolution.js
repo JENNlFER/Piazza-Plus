3a12d1015123a7e35abe4d3ddd7acf4d546162f6
EileenTheCrow <eileencatoe@gmail.com>
2021-12-08 23:03:51 -0500

    Finished disabling resolved function

--- plugin/resolution.js
+++ plugin/resolution.js
@@ -1,11 +1,24 @@
+function disableRadios(){
+  let unresolvedElements = document.getElementsByClassName("mark_unresolved");
+  let resolvedElements = document.getElementsByClassName("mark_resolved");
 
-let unresolvedElements = document.getElementsByClassName("mark_unresolved");
-let resolvedElements = document.getElementsByClassName("mark_resolved");
+  for(let i = 0; i < unresolvedElements.length; ++i){
+    unresolvedElements[i].disabled = true;
+  }
 
-for(let i = 0; i < unresolvedElements.length; ++i){
-  unresolvedElements[i].disabled = true;
+  for(let j = 0; j < resolvedElements.length; ++j){
+    resolvedElements[j].disabled = true;
+  }
 }
 
-for(let j = 0; j < resolvedElements.length; ++j){
-  resolvedElements[j].disabled = true;
+function callback(){
+  disableRadios();
 }
+
+const observer = new MutationObserver(callback);
+let view = document.getElementById("views");
+observer.observe(view, {
+  childList: true,
+  subtree: true
+});
+
