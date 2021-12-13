48acb7b25faae9b01c21c82d33fa4e8bcacb4600
Jennifer <jteissler@uga.edu>
2021-12-13 06:49:59 -0500

    preview selector done

--- plugin/posts-restyle.js
+++ plugin/posts-restyle.js
@@ -2,10 +2,12 @@ const panel = document.getElementsByClassName('main_panel')[0];
 
 const orb_callback = function(mutations, observer) {
 	observer.disconnect();
-	const actions = document.getElementsByClassName('actions');
+	let actions = [...document.getElementsByClassName('actions'), ...document.getElementsByClassName('post_region_actions')];
 	for (const a of actions) {
 		if (a.getElementsByClassName('orb').length > 0) continue;
-		
+		let preview = a.parentElement.querySelector('.no-preview') == null;
+
+
 		const links = a.getElementsByClassName('preview_link');
 		for (const link of links) {
 			let preview_span = document.createElement('SPAN');
@@ -13,9 +15,8 @@ const orb_callback = function(mutations, observer) {
 			preview_span.classList.add('orb-label');
 			a.insertBefore(preview_span, link);
 			link.classList.add('orb');
-			// TODO: Fix me so I apply the correct on/off state
+			if (preview) link.classList.add('on');
 			link.addEventListener('click', () => {
-				console.log('clicked, bitch')
 				if (link.classList.contains('on')) {
 					link.classList.remove('on');
 				} else {
