b436471bba1aae26bf03dac4e8b7e56befd58c60
Jennifer <jteissler@uga.edu>
2021-12-13 17:41:50 -0500

    allow users to resolve own posts

--- plugin/resolution.js
+++ plugin/resolution.js
@@ -1,13 +1,26 @@
 function disableRadios(){
   let unresolvedElements = document.getElementsByClassName("mark_unresolved");
   let resolvedElements = document.getElementsByClassName("mark_resolved");
+  let user_self = document.querySelector('.my_name').innerText;
 
   for(let i = 0; i < unresolvedElements.length; ++i){
-    unresolvedElements[i].disabled = true;
+    let user_post = unresolvedElements[i].parentElement.parentElement.parentElement.parentElement.querySelector('.user_name').innerText;
+
+    if (user_post != user_self) {
+      unresolvedElements[i].disabled = true;
+    } else {
+      unresolvedElements[i].disabled = false;
+    }
   }
 
   for(let j = 0; j < resolvedElements.length; ++j){
-    resolvedElements[j].disabled = true;
+    let user_post = resolvedElements[j].parentElement.parentElement.parentElement.parentElement.querySelector('.user_name').innerText;
+    
+    if (user_post != user_self) {
+      resolvedElements[j].disabled = true;
+    } else {
+      resolvedElements[j].disabled = false;
+    }
   }
 }
 
